b0d6b621aafa1829cab5b39f8902583640bcebda
rahulkandpal <rkandpal.47@gmail.com>
2022-05-13 23:06:04 +0530

    first commit

---
+++ vars/main.groovy
@@ -0,0 +1,25 @@
+def call()
+{
+  pipeline{
+    agent any
+    stages{
+      stage("build")
+      {
+        steps{
+        }
+      }
+      
+      stage("test")
+      {
+        steps{
+        }
+      }
+      
+      stage("deploy")
+      {
+        steps{
+        }
+      }
+    }
+  }
+}
